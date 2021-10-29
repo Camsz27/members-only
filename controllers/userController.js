b126fa558f147719e9f3d8e8f39446612adf6a82
Camsz27 <camiloagr2707@gmail.com>
2021-10-28 21:36:45 -0500

    Users can now sign up, password is hashed and user data is stored in the db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,58 @@
 const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 exports.sign_up_get = function (req, res, next) {
   res.render('sign_up', { title: 'Sign up' });
 };
 
-exports.sign_up_post = function (req, res, next) {
-  res.send('back on track');
-};
+exports.sign_up_post = [
+  body('username')
+    .trim()
+    .isAlpha()
+    .withMessage('Only alpha characters are allowed')
+    .isLength({ min: 4, max: 30 })
+    .withMessage('Username has to be between 4 and 30 characters')
+    .escape(),
+
+  body('password')
+    .isLength({ min: 7 })
+    .withMessage('Password must have at least 7 characters')
+    .custom((value, { req }) => value === req.body.confirmPassword)
+    .withMessage('Passwords do not match'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('sign_up', {
+        title: 'Sign up',
+        username: req.body.username,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      if (err) {
+        res.render('error');
+      }
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+        admin: false,
+        member: false,
+      });
+
+      user.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/login', { title: 'Login' });
+      });
+    });
+  },
+];
 
 exports.login_get = function (req, res, next) {
   res.render('login', { title: 'Login' });
